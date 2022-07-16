3d6c313a2f6b632bad3022f9912c79ea5997162d
DennisWanjeri <shikandadennis07@gmail.com>
2022-07-16 20:04:30 +0200

    populating!

---
+++ Backend/first_project/populate_first_app.py
@@ -0,0 +1,51 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_project.settings')
+
+import django
+django.setup()
+
+import random
+from first_app.models import AccessRecord, Webpage, Topic
+from faker import Faker
+
+
+
+print("starting")
+
+
+
+
+# Fake pop script
+
+fakegen = Faker()
+topics = ['Search', 'Social', 'Marketplace', 'News', 'Games']
+
+
+def add_topic():
+    t = Topic.objects.get_or_create(top_name=random.choice(topics))[0]
+    t.save()
+    return t
+
+
+def populate(N=5):
+    for entry in range(N):
+        # get the topic for the entry
+        top = add_topic()
+
+        # create fake data for that entry
+        fake_url = fakegen.url()
+        fake_date = fakegen.date()
+        fake_name = fakegen.company()
+
+        webpg = Webpage.objects.get_or_create(
+            topic=top, url=fake_url, name=fake_name)[0]
+
+        # create a fake access record for that webpage
+        acc_rec = AccessRecord.objects.get_or_create(
+            name=webpg, date=fake_date)
+
+
+if __name__ == '__main__':
+    print("Populating script!")
+    populate(20)
+    print("populating complete")
