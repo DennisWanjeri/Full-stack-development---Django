e7a41de0cafb67b8da7f697f6c1009405186a8a3
DennisWanjeri <shikandadennis07@gmail.com>
2022-07-18 11:02:14 +0200

    django!

--- Backend/second_project/second_app/views.py
+++ Backend/second_project/second_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from second_app.models import User
+# from second_app.models import User
+from second_app.forms import NewUserForm
 # Create your views here.
 
 
@@ -8,6 +9,15 @@ def index(request):
 
 
 def users(request):
-    user_list = User.objects.order_by('first_name')
-    user_dict = {'users': user_list}
-    return render(request, 'second_app/users.html', context=user_dict)
+
+    form = NewUserForm()
+
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('Error Form invalid')
+    return render(request, 'second_app/users.html', {'form': form})
