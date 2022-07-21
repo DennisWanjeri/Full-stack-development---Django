b00d25787532c4a65da3abe4d4bb0048e68c5cf9
DennisWanjeri <shikandadennis07@gmail.com>
2022-07-21 15:14:05 +0200

    foundations on django completed!

--- advanced_django/advanced/basic_app/views.py
+++ advanced_django/advanced/basic_app/views.py
@@ -3,7 +3,7 @@ from django.views.generic import (View, TemplateView,
                                   ListView, DetailView,
                                   CreateView, UpdateView, DeleteView)
 from . import models
-
+from django.core.urlresolvers import reverse_lazy
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -23,3 +23,13 @@ class SchoolDetailView(DetailView):
 class SchoolCreateView(CreateView):
     fields = ('name', 'principal', 'location')
     model = models.School
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name', 'principal')
+    model = models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
+    
